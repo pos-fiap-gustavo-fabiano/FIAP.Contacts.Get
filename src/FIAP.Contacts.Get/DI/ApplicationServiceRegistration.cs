aad56e78c1b0673a7a871f8479e69d089932352b
Fabiano Teixeira <fabianotxss@gmail.com>
2025-03-22 14:03:29 -0300

    adicionando open telemetry

--- src/FIAP.Contacts.Get/DI/ApplicationServiceRegistration.cs
+++ src/FIAP.Contacts.Get/DI/ApplicationServiceRegistration.cs
@@ -4,6 +4,9 @@ using Serilog;
 using Serilog.Extensions.Logging;
 using System.Diagnostics.Metrics;
 using System.Diagnostics;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace FIAP.Contacts.Get.DI;
 
@@ -12,14 +15,47 @@ public static class ApplicationServiceRegistration
     public static IServiceCollection AddFunctionService(this IServiceCollection services)
     {
 
-        var loggerConfig = new LoggerConfiguration()
-            .Enrich.FromLogContext()
-            .Enrich.WithProperty("ApplicationName", "FIAP.Contacts.Get.Api")
-            .WriteTo.Console()
-            .CreateLogger();
+        var serviceName = "fiap-contact-function-get";
+        var serviceVersion = "1.0.0";
 
-        services.AddSingleton<ILoggerFactory>(new SerilogLoggerFactory(loggerConfig));
-        services.AddLogging();
+        // Create a single ActivitySource that can be used throughout the application
+        var activitySource = new ActivitySource(serviceName, serviceVersion);
+        services.AddSingleton(activitySource);
+
+        var resourceBuilder = ResourceBuilder.CreateDefault()
+          .AddService(serviceName: serviceName, serviceVersion: serviceVersion);
+
+        services.AddOpenTelemetry()
+               .ConfigureResource(resource => resource.AddService(
+                   serviceName: serviceName,
+                   serviceVersion: serviceVersion))
+               .WithTracing(tracing => tracing
+                   .AddSource(serviceName)
+                   .AddAspNetCoreInstrumentation()
+                   .AddHttpClientInstrumentation()
+                   .AddEntityFrameworkCoreInstrumentation(x => x.SetDbStatementForText = true) // Add if using EF Core
+                   .AddOtlpExporter(options =>
+                   {
+                       options.Endpoint = new Uri("http://134.122.121.176:4317");
+                       options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+                   }));
+
+        // Configure OpenTelemetry logging
+        services.AddLogging(logging =>
+        {
+            logging.AddOpenTelemetry(options =>
+            {
+                options.SetResourceBuilder(resourceBuilder);
+                options.IncludeFormattedMessage = true;
+                options.IncludeScopes = true;
+
+                options.AddOtlpExporter(exporterOptions =>
+                {
+                    exporterOptions.Endpoint = new Uri("http://134.122.121.176:4317");
+                    exporterOptions.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+                });
+            });
+        });
 
         return services;
     }
