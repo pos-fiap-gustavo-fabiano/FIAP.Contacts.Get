2e6f2bc453b4554d6c6764bbcbb9d8e0980127a6
Gustavo Rodrigues <gustavo.rodrigues@sgrentals.com.br>
2025-03-23 15:59:54 -0300

    add projeto test

---
+++ tests/FIAP.Contacts.Get.Tests/Domain/Mock/AddressMock.cs
@@ -0,0 +1,19 @@
+﻿using Bogus;
+using FIAP.Contacts.Get.Domain.ContactAggregate;
+
+namespace FIAP.Contacts.Get.Tests.Domain.Mock;
+
+public class AddressMock
+{
+    private static readonly Faker _faker = new("pt_BR");
+
+    public static Address Create() =>
+        new Address(
+        _faker.Address.StreetName(),
+        _faker.Address.BuildingNumber(),
+        _faker.Address.City(),
+        _faker.Address.City(),
+        _faker.Address.StateAbbr(),
+        _faker.Address.ZipCode("########"),
+        _faker.Address.SecondaryAddress());
+}
---
+++ tests/FIAP.Contacts.Get.Tests/Domain/Mock/PhoneMock.cs
@@ -0,0 +1,13 @@
+﻿using Bogus;
+using FIAP.Contacts.Get.Domain.ContactAggregate;
+
+namespace FIAP.Contacts.Get.Tests.Domain.Mock;
+
+public static class PhoneMock
+{
+    private static readonly Faker _faker = new("pt_BR");
+
+    public static Phone Create() =>
+        new Phone(new Random().Next(1, 99), _faker.Phone.PhoneNumber());
+
+}
